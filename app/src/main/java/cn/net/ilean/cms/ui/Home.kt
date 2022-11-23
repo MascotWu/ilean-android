cae4b5f543bc473ced3d361dfd8458aa4b059387
Violet Rosalia <violet.rosalia@gmail.com>
2022-11-23 11:35:52 +0800

    新增BottomBar

--- app/src/main/java/cn/net/ilean/cms/ui/Home.kt
+++ app/src/main/java/cn/net/ilean/cms/ui/Home.kt
@@ -6,13 +6,15 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -34,6 +36,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 import java.util.concurrent.TimeUnit
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Companies(navigate: (Int) -> Unit) {
     val total = remember { mutableStateOf<Int?>(null) }
@@ -50,11 +53,8 @@ fun Companies(navigate: (Int) -> Unit) {
         val today: Calendar = Calendar.getInstance(Locale.CHINA)
         today.add(Calendar.MONTH, -3)
         companyService.getCompanies(
-            pageNum = 1,
-            orderBy = radioOptions[key]!!,
-            lastUsedTime = SimpleDateFormat(
-                "yyyy-MM-dd",
-                Locale.CHINA
+            pageNum = 1, orderBy = radioOptions[key]!!, lastUsedTime = SimpleDateFormat(
+                "yyyy-MM-dd", Locale.CHINA
             ).format(Date(today.timeInMillis))
         ).enqueue(object : Callback<Wrapper<Page<Company>>> {
             override fun onResponse(
@@ -73,130 +73,137 @@ fun Companies(navigate: (Int) -> Unit) {
     }
     onOptionSelected(selectedOption.value)
     val dismiss = remember { mutableStateOf(true) }
-    if (!dismiss.value)
-        Dialog(onDismissRequest = { dismiss.value = true }, content = {
-            Column(
-                modifier = Modifier
-                    .clip(RoundedCornerShape(8.dp))
-                    .background(Color.White)
-                    .padding(8.dp)
-            ) {
-                radioOptions.keys.forEach { key ->
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .clickable {
-                                onOptionSelected(key)
-                                dismiss.value = true
-                            }) {
-                        RadioButton(
-                            selected = key == selectedOption.value,
-                            onClick = {
-                                onOptionSelected(key)
-                                dismiss.value = true
-                            })
-                        Text(key, fontSize = 16.sp)
-                    }
+    if (!dismiss.value) Dialog(onDismissRequest = { dismiss.value = true }, content = {
+        Column(
+            modifier = Modifier
+                .clip(RoundedCornerShape(8.dp))
+                .background(Color.White)
+                .padding(8.dp)
+        ) {
+            radioOptions.keys.forEach { key ->
+                Row(verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .clickable {
+                            onOptionSelected(key)
+                            dismiss.value = true
+                        }) {
+                    RadioButton(selected = key == selectedOption.value, onClick = {
+                        onOptionSelected(key)
+                        dismiss.value = true
+                    })
+                    Text(key, fontSize = 16.sp)
                 }
             }
+        }
+    })
+    Scaffold(topBar = {
+        CenterAlignedTopAppBar(title = {
+            Text(
+                "Companies",
+                style = MaterialTheme.typography.labelLarge,
+            )
         })
-    Column(modifier = Modifier.padding(vertical = 2.dp)) {
-        Column(modifier = Modifier.padding(horizontal = 8.dp)) {
-            TextButton(onClick = { dismiss.value = false }) { Text(selectedOption.value) }
-            if (total.value != null)
-                Text(
+    }, bottomBar = {
+        BottomAppBar(backgroundColor = Color.White) {
+            var checkedTab by remember { mutableStateOf(0) }
+            listOf(Icons.Filled.Home, Icons.Filled.AccountBox).forEachIndexed { i, imageVector ->
+                IconToggleButton(checked = checkedTab == i, onCheckedChange = { checkedTab = i }) {
+                    Icon(
+                        imageVector = imageVector,
+                        tint = if (checkedTab == i) Color.Magenta else Color.Gray,
+                        contentDescription = "Home"
+                    )
+                }
+            }
+        }
+    }) {
+        Column(modifier = Modifier.padding(vertical = 2.dp)) {
+            Column(modifier = Modifier.padding(horizontal = 8.dp)) {
+                TextButton(onClick = { dismiss.value = false }) { Text(selectedOption.value) }
+                if (total.value != null) Text(
                     "总数 ${total.value}",
                     Modifier.padding(start = 8.dp, bottom = 4.dp, end = 8.dp, top = 2.dp),
                     fontSize = 14.sp,
                     style = TextStyle(color = Color.Gray),
                 )
-        }
+            }
 
-        LazyColumn {
-            items(items = companies, itemContent = { company: Company ->
-                Column(
-                    modifier = Modifier
+            LazyColumn {
+                items(items = companies, itemContent = { company: Company ->
+                    Column(modifier = Modifier
                         .clickable { navigate(company.companyId!!) }
-                        .padding(horizontal = 16.dp, vertical = 6.dp)
-                ) {
-                    Row(modifier = Modifier.fillMaxWidth()) {
-                        Text(
-                            company.name ?: "",
-                            maxLines = 2,
-                            overflow = TextOverflow.Ellipsis,
-                            fontSize = 18.sp,
-                        )
-                        Text(
-                            text = " #${company.companyId}",
-                            maxLines = 1,
-                            fontSize = 18.sp,
-                            style = TextStyle(color = Color.Gray)
-                        )
-                    }
-                    Row(
-                        modifier = Modifier.fillMaxWidth(),
-                        horizontalArrangement = Arrangement.Start
-                    ) {
-                        Row(modifier = Modifier.padding(top = 4.dp, end = 12.dp)) {
+                        .padding(horizontal = 16.dp, vertical = 6.dp)) {
+                        Row(modifier = Modifier.fillMaxWidth()) {
                             Text(
-                                "问题 ",
-                                style = TextStyle(color = Color.Gray),
-                                fontSize = 14.sp
+                                company.name ?: "",
+                                maxLines = 2,
+                                overflow = TextOverflow.Ellipsis,
+                                fontSize = 18.sp,
                             )
                             Text(
-                                "${company.countOfIssue}",
-                                style = TextStyle(color = Color.Blue),
-                                fontSize = 14.sp
+                                text = " #${company.companyId}",
+                                maxLines = 1,
+                                fontSize = 18.sp,
+                                style = TextStyle(color = Color.Gray)
                             )
                         }
-                        Row(modifier = Modifier.padding(top = 4.dp, end = 12.dp)) {
-                            Text(
-                                "员工 ",
-                                style = TextStyle(color = Color.Gray),
-                                fontSize = 14.sp
-                            )
-                            Text(
-                                "${company.countOfEmployees}",
-                                style = TextStyle(color = Color.Blue),
-                                fontSize = 14.sp
-                            )
-                        }
-                    }
-                    Column(modifier = Modifier.padding(top = 4.dp)) {
-                        Text(
-                            "注册于 " + company.dateCreated,
-                            style = TextStyle(color = Color.Gray),
-                            fontSize = 14.sp
-                        )
-                        if (company.lastUsedTime != null) {
-                            val lastUsedTime = SimpleDateFormat(
-                                "yyyy-MM-dd", Locale.CHINA
-                            ).parse(company.lastUsedTime!!)
-                            val numberOfDaysInactive = TimeUnit.DAYS.convert(
-                                Date().time - lastUsedTime!!.time,
-                                TimeUnit.MILLISECONDS
-                            )
-                            if (numberOfDaysInactive < 30) {
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.Start
+                        ) {
+                            Row(modifier = Modifier.padding(top = 4.dp, end = 12.dp)) {
                                 Text(
-                                    "${numberOfDaysInactive}天前使用过",
-                                    style = TextStyle(color = Color.Gray),
+                                    "问题 ", style = TextStyle(color = Color.Gray), fontSize = 14.sp
+                                )
+                                Text(
+                                    "${company.countOfIssue}",
+                                    style = TextStyle(color = Color.Blue),
                                     fontSize = 14.sp
                                 )
-                            } else {
+                            }
+                            Row(modifier = Modifier.padding(top = 4.dp, end = 12.dp)) {
                                 Text(
-                                    "超过${numberOfDaysInactive}天未使用",
-                                    style = TextStyle(
-                                        color = Color.Red,
-                                        fontWeight = FontWeight.W600
-                                    ),
+                                    "员工 ", style = TextStyle(color = Color.Gray), fontSize = 14.sp
+                                )
+                                Text(
+                                    "${company.countOfEmployees}",
+                                    style = TextStyle(color = Color.Blue),
                                     fontSize = 14.sp
                                 )
                             }
                         }
+                        Column(modifier = Modifier.padding(top = 4.dp)) {
+                            Text(
+                                "注册于 " + company.dateCreated,
+                                style = TextStyle(color = Color.Gray),
+                                fontSize = 14.sp
+                            )
+                            if (company.lastUsedTime != null) {
+                                val lastUsedTime = SimpleDateFormat(
+                                    "yyyy-MM-dd", Locale.CHINA
+                                ).parse(company.lastUsedTime!!)
+                                val numberOfDaysInactive = TimeUnit.DAYS.convert(
+                                    Date().time - lastUsedTime!!.time, TimeUnit.MILLISECONDS
+                                )
+                                if (numberOfDaysInactive < 30) {
+                                    Text(
+                                        "${numberOfDaysInactive}天前使用过",
+                                        style = TextStyle(color = Color.Gray),
+                                        fontSize = 14.sp
+                                    )
+                                } else {
+                                    Text(
+                                        "超过${numberOfDaysInactive}天未使用", style = TextStyle(
+                                            color = Color.Red, fontWeight = FontWeight.W600
+                                        ), fontSize = 14.sp
+                                    )
+                                }
+                            }
+                        }
                     }
-                }
-            })
+                })
+            }
         }
     }
 }

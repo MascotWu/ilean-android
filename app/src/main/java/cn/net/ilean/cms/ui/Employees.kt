c543e1b205b0b7655eff27aadba31b1b13738ec3
Violet Rosalia <violet.rosalia@gmail.com>
2023-01-11 01:19:55 +0800

    重构员工列表页面

--- app/src/main/java/cn/net/ilean/cms/ui/Employees.kt
+++ app/src/main/java/cn/net/ilean/cms/ui/Employees.kt
@@ -1,17 +1,24 @@
 package cn.net.ilean.cms.ui
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewModelScope
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.LoadState
@@ -21,8 +28,10 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import androidx.paging.compose.items
 import cn.net.ilean.cms.LeanDestination
 import cn.net.ilean.cms.LeanNavigationActions
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
 @Composable
 fun Employees(navigationActions: LeanNavigationActions, mainViewModel: MainViewModel) {
     val scaffoldState = rememberScaffoldState()
@@ -68,72 +77,72 @@ fun Employees(navigationActions: LeanNavigationActions, mainViewModel: MainViewM
         },
     ) {
         println("<top>.Employees 切换之后，这里会重新执行")
-        var dialogVisible by remember { mutableStateOf(false) }
-        if (dialogVisible) Dialog(onDismissRequest = { dialogVisible = false }) {
-            Column(Modifier.background(MaterialTheme.colors.background)) {
-                radioOptions.entries.forEach {
-                    Row {
-                        RadioButton(selected = option.key == it.key, onClick = {
-                            option = it; dialogVisible = false;
-                            println("option = ${option.value}")
-                            pager = Pager(
-                                PagingConfig(
-                                    initialLoadSize = 20,
-                                    pageSize = 20,
-                                    enablePlaceholders = true,
-                                )
-                            ) {
-                                mainViewModel.users(option.value)
-                            }
-                        })
-                        Text(text = it.key)
+        val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
+        LazyColumn(modifier = Modifier.padding(top = 8.dp)) {
+            stickyHeader {
+                Column {
+                    Row(
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier
+                            .padding(horizontal = 12.dp, vertical = 2.dp)
+                            .horizontalScroll(rememberScrollState())
+                    ) {
+                        radioOptions.entries.forEach { entry ->
+                            FilterChip(
+                                selected = option.key == entry.key,
+                                onClick = {
+                                    option = entry
+                                    pager = Pager(
+                                        PagingConfig(
+                                            initialLoadSize = 20,
+                                            pageSize = 20,
+                                            enablePlaceholders = true,
+                                        )
+                                    ) {
+                                        mainViewModel.users(option.value)
+                                    }
+                                },
+                                label = { Text(entry.key) })
+                        }
                     }
                 }
             }
-        }
-        Column {
-            TextButton(onClick = { dialogVisible = true }) {
-                Text(option.key)
-            }
 
-            val lazyPagingItems = pager.flow.collectAsLazyPagingItems()
-            LazyColumn(modifier = Modifier.padding(top = 8.dp)) {
-                if (lazyPagingItems.loadState.refresh == LoadState.Loading) {
-                    item {
-                        Text(
-                            text = "Waiting for items to load from the backend",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .wrapContentWidth(Alignment.CenterHorizontally)
-                        )
-                    }
-                }
-
-                items(lazyPagingItems) { item ->
-                    Column(
+            if (lazyPagingItems.loadState.refresh == LoadState.Loading) {
+                item {
+                    Text(
+                        text = "Waiting for items to load from the backend",
                         modifier = Modifier
                             .fillMaxWidth()
-                            .padding(horizontal = 12.dp, vertical = 8.dp)
-                    ) {
-                        Text("${item?.name}", fontSize = 20.sp)
-                        if (!item?.phone.isNullOrBlank()) Text("${item?.phone}", fontSize = 18.sp)
-                        Text("上次登录 ${item?.lastLoginTime}", fontSize = 18.sp)
-                        Text("记录问题 ${item?.countOfIssueCreated}", fontSize = 18.sp)
-                        Text("解决问题 ${item?.countOfIssueSolved}", fontSize = 18.sp)
-                        if (!item?.registerTime.isNullOrBlank())
-                            Text("注册时间 ${item?.registerTime ?: "(空)"}", fontSize = 18.sp)
-                    }
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                    )
                 }
+            }
 
-                if (lazyPagingItems.loadState.append == LoadState.Loading) {
-                    item {
-                        CircularProgressIndicator(
-                            modifier = Modifier
-                                .padding(8.dp)
-                                .fillMaxWidth()
-                                .wrapContentWidth(Alignment.CenterHorizontally)
-                        )
-                    }
+            items(lazyPagingItems) { item ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 12.dp, vertical = 8.dp)
+                ) {
+                    Text("${item?.name}", fontSize = 20.sp)
+                    if (!item?.phone.isNullOrBlank()) Text("${item?.phone}", fontSize = 18.sp)
+                    Text("上次登录 ${item?.lastLoginTime}", fontSize = 18.sp)
+                    Text("记录问题 ${item?.countOfIssueCreated}", fontSize = 18.sp)
+                    Text("解决问题 ${item?.countOfIssueSolved}", fontSize = 18.sp)
+                    if (!item?.registerTime.isNullOrBlank())
+                        Text("注册时间 ${item?.registerTime ?: "(空)"}", fontSize = 18.sp)
+                }
+            }
+
+            if (lazyPagingItems.loadState.append == LoadState.Loading) {
+                item {
+                    CircularProgressIndicator(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth()
+                            .wrapContentWidth(Alignment.CenterHorizontally)
+                    )
                 }
             }
         }
